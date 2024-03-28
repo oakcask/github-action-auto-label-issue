9ac509ddd31ddbdd6f8f778c0bed4d86d5aa896a
Kuya Kohara <kuya.kohara@gmail.com>
2024-03-29 06:30:17 +0900

    chore: let eslint scan ts files (#406)

--- src/issue.ts
+++ src/issue.ts
@@ -1,23 +1,22 @@
-import { Github, queryNextIssueLabels } from "./github";
+import { Github, queryNextIssueLabels } from './github'
 
-export async function enumerateIssueLabels(gh: Github, { repo, owner, issueNumber }: { repo: string, owner: string, issueNumber: number }): Promise<string[]> {
+export async function enumerateIssueLabels (gh: Github, { repo, owner, issueNumber }: { repo: string, owner: string, issueNumber: number }): Promise<string[]> {
   const labels: string[] = []
-  let lastEndCursor: string | undefined = undefined
+  let lastEndCursor: string | undefined
   let done = false
 
   while (!done) {
     const res = await queryNextIssueLabels(gh, {
-      repo, owner, issueNumber, pageSize: 100, lastEndCursor,
-    });
+      repo, owner, issueNumber, pageSize: 100, lastEndCursor
+    })
     if (!res.repository?.issue?.labels?.nodes) {
-      throw Error("failed to query issue labels")
+      throw Error('failed to query issue labels')
     }
     const { endCursor, hasNextPage } = res.repository.issue.labels.pageInfo
-    for (let node of res.repository.issue.labels.nodes) {
+    for (const node of res.repository.issue.labels.nodes) {
       if (node?.name) {
         labels.push(node.name)
       }
-      node?.name
     }
 
     lastEndCursor = endCursor ?? undefined
