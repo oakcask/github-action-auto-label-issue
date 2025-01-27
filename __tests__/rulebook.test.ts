993e0a6e01ecfe34c814338e44909113d8c2e63a
Kuya KOHARA <kuya.kohara@gmail.com>
2025-01-28 01:30:19 +0900

    refactor: implement rules-and-actions model
    closes #589

---
+++ __tests__/rulebook.test.ts
@@ -0,0 +1,125 @@
+import { describe, it, expect, vi } from 'vitest'
+import type { Rule } from '../src/rulebook-types.js'
+import { executeRulebook, loadLegacyRule, loadRulebook } from '../src/rulebook'
+import type { Expression } from '../src/ghimex-types'
+import { Context } from '../src/context'
+
+describe('loadRulebook', () => {
+  it('loads examples/rulebook.yaml', async () => {
+    const rulebook = await loadRulebook({ path: 'examples/rulebook.yaml' })
+    expect(rulebook).not.toBeUndefined()
+    expect(rulebook).toStrictEqual([
+      {
+        id: 'detect-caribbean-pirate-in-issue',
+        when: [
+          { label: 'label-test' },
+          {
+            all: [
+              { label: 'label-test:rum' },
+              {
+                any: [
+                  '[Aa]hoy',
+                  '[Mm]atey',
+                  '([AR]a*|Ya+)rrr+!'
+                ]
+              }
+            ]
+          }
+        ],
+        then: [
+          { addLabel: 'label-test:pirate:caribbean' }
+        ]
+      },
+      {
+        id: 'reverse-detect-caribbean-pirate-in-issue',
+        when: [
+          { label: 'label-test' },
+          {
+            not: [
+              { label: 'label-test:rum' },
+              {
+                any: [
+                  '[Aa]hoy',
+                  '[Mm]atey',
+                  '([AR]a*|Ya+)rrr+!'
+                ]
+              }
+            ]
+          }
+        ],
+        then: [
+          { removeLabel: 'label-test:pirate:caribbean' }
+        ]
+      }
+    ])
+  })
+})
+
+describe('loadLegacyRule', () => {
+  it('loads Rulebook from legacy Configuration', () => {
+    const expression: Expression = {
+      all: [
+        { label: 'rum' },
+        {
+          any: [
+            '[Aa]hoy',
+            '[Mm]atey',
+            '([AR]a*|Ya+)rrr+!'
+          ]
+        }
+      ]
+    }
+    const got = loadLegacyRule({
+      'pirate:caribbean': {
+        removeOnMissing: true,
+        expression
+      }
+    })
+    expect(got).toStrictEqual([
+      {
+        when: expression,
+        then: [{ addLabel: 'pirate:caribbean' }]
+      },
+      {
+        when: { not: expression },
+        then: [{ removeLabel: 'pirate:caribbean' }]
+      }
+    ])
+  })
+})
+
+describe('executeRulebook', () => {
+  it('invokes onAddLabel when condition matches', async () => {
+    const onAddLabel = vi.fn()
+    const ctx: Context = {
+      body: () => 'a',
+      labels: () => [],
+      onAddLabel,
+      onRemoveLabel: () => {},
+      finish: () => Promise.resolve()
+    }
+    const rule: Rule = {
+      when: 'a',
+      then: [{ addLabel: 'added' }]
+    }
+    await executeRulebook(ctx, rule)
+    expect(onAddLabel).toHaveBeenCalledWith('added')
+  })
+
+  it('invokes onRemoveLabel when condition matches', async () => {
+    const onRemoveLabel = vi.fn()
+    const ctx: Context = {
+      body: () => 'b',
+      labels: () => [],
+      onAddLabel: () => {},
+      onRemoveLabel,
+      finish: () => Promise.resolve()
+    }
+    const rule: Rule = {
+      when: { not: 'a' },
+      then: [{ removeLabel: 'added' }]
+    }
+    await executeRulebook(ctx, rule)
+    expect(onRemoveLabel).toHaveBeenCalledWith('added')
+  })
+})
---
+++ src/rulebook.ts
@@ -0,0 +1,90 @@
+import fs from 'node:fs/promises'
+import util from 'node:util'
+import * as core from '@actions/core'
+import yaml from 'js-yaml'
+import SCHEMA_JSON from './generated/rulebook.schema.json' with { 'type': 'json' }
+import type { Schema, Rule, Action } from './rulebook-types.js'
+import { Ajv } from 'ajv'
+import type { Configuration } from './config.js'
+import type { Context } from './context.js'
+import { isMatch } from './ghimex.js'
+
+const schema = new Ajv({ loadSchema: async () => ({}) })
+  .compileAsync<Schema>(SCHEMA_JSON)
+
+type LoadOption
+  = { path: string }
+  | string
+
+export async function loadRulebook (option: LoadOption): Promise<Schema | undefined> {
+  if (typeof option === 'object' && 'path' in option) {
+    return loadRulebook(await fs.readFile(option.path, 'utf8'))
+  }
+  const validate = await schema
+  const json = yaml.load(option)
+  if (validate(json)) {
+    return json
+  }
+  if (validate.errors) {
+    for (const e of validate.errors) {
+      core.error(util.format('%o', e))
+    }
+  }
+}
+
+export function loadLegacyRule (configuration: Configuration): Schema {
+  const rules: Rule[] = []
+
+  for (const label in configuration) {
+    const { removeOnMissing, expression } = configuration[label]
+
+    rules.push({
+      when: expression,
+      then: [{ addLabel: label }]
+    })
+    if (removeOnMissing) {
+      rules.push({
+        when: { not: expression },
+        then: [{ removeLabel: label }]
+      })
+    }
+  }
+
+  return rules
+}
+
+function executeAction (context: Context, action: Action) {
+  if ('addLabel' in action) {
+    context.onAddLabel(action.addLabel)
+  }
+  if ('removeLabel' in action) {
+    context.onRemoveLabel(action.removeLabel)
+  }
+}
+
+function executeRule (context: Context, rule: Rule) {
+  const { id, when: condition, then: actions } = rule
+
+  if (isMatch(context, condition)) {
+    if (id) {
+      core.debug(util.format('rule(id: %s) matched', id))
+    }
+    if (actions) {
+      for (const action of actions) {
+        executeAction(context, action)
+      }
+    }
+  }
+}
+
+export async function executeRulebook (context: Context, rule: Schema) {
+  if (Array.isArray(rule)) {
+    for (const r of rule) {
+      executeRule(context, r)
+    }
+  } else {
+    executeRule(context, rule)
+  }
+
+  await context.finish()
+}
